56619967bbb9079b611bfa6343740500c85dbd5d
Fernando Oliveira <fernandooliveira@live.com>
2024-08-05 12:43:12 -0300

    .

---
+++ exercicio01/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int x;
+        System.out.print("Digite um valor inteiro para X: ");
+        x = sc.nextInt();
+        System.out.println();
+
+        int y;
+        System.out.print("Digite um valor inteiro para Y: ");
+        y = sc.nextInt();
+        System.out.println();
+
+        int z;
+        z = x+y;
+
+        System.out.println("SOMA = " + z);
+        System.out.println();
+        sc.close();
+        
+    }
+}
