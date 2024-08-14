bfc46ab934b99f8e66ff9a0dd7341d42bbd5e134
Fernando Oliveira <fernandooliveira@live.com>
2024-08-14 08:36:26 -0300

    exercicio05

--- estrutura-condicional/exercicio05/Main.java
+++ estrutura-condicional/exercicio05/Main.java
@@ -7,24 +7,26 @@ public class Main {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        int x;
+        int x, y;
         System.out.print("Digite o código do item: ");
         x = sc.nextInt();
+        System.out.print("Digite a quantidade de itens: ");
+        y = sc.nextInt();
 
         if (x == 1) {
-            System.out.printf("Total: R$ %.2f %n", (x*4.00));
+            System.out.printf("Total: R$ %.2f %n", (y*4.00));
         }
         else if (x == 2) {
-            System.out.printf("Total: R$ %.2f %n", (x*4.50));
+            System.out.printf("Total: R$ %.2f %n", (y*4.50));
         }
         else if (x == 3) {
-            System.out.printf("Total: R$ %.2f %n", (x*5.00));
+            System.out.printf("Total: R$ %.2f %n", (y*5.00));
         }
         else if (x == 4) {
-            System.out.printf("Total: R$ %.2f %n", (x*2.00));
+            System.out.printf("Total: R$ %.2f %n", (y*2.00));
         }
         else if (x == 5) {
-            System.out.printf("Total: R$ %.2f %n", (x*1.50));
+            System.out.printf("Total: R$ %.2f %n", (y*1.50));
         }
         sc.close();
     }
